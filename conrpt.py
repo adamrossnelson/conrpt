3e86c0815ae8e15f1ed843adc12972058e853416
adamrossnelson <adamrossnelson@gmail.com>
2018-05-08 14:37:28 -0500

    Working test code

--- conrpt.py
+++ conrpt.py
@@ -5,24 +5,47 @@
 
 def conrpt(rvar, tvar):
     from pandas import DataFrame
+    import pandas
 
-    df = DataFrame(rvar, tvar, columns=['rvar','tvar'])
+    print('Printing rvar')
+    print(rvar)
+
+    print('Printing tvar')
+    print(tvar)
+
+    # df = DataFrame(rvar, tvar, columns=['rvar','tvar'])
+    # df = pandas.merge(rvar, tvar, how='outer')
+    df = pandas.concat([rvar.to_frame(), tvar.to_frame()], 
+       axis=1, ignore_index=True)
+    df.columns = ['rvar','tvar']
+
+    # df = DataFrame(rvar.to_frame(), tvar.to_frame(), columns=['rvar','tvar'])
+
+    print('Printing df')
+    print(df)
+
+    df_xtab = pandas.crosstab(df['rvar'], df['tvar'])
+
+    print('printing df_xtab')
+    print(df_xtab)
 
     # print(df['rvar'].isin([1,0]).all())
     # print(df['tvar'].isin([1,0]).all())
 
     # Test that arguments are binary
-    if not (df['rvar'].isin([1,0]).all() or not df['tvar'].isin([1,0]).all()):
+    # if not (df['rvar'].isin([1,0]).all()) or not (df['tvar'].isin([1,0]).all()):
+    # if ((df['rvar'].isin([1,0]).all() is not True) or (df['tvar'].isin([1,0]).all() is not True)):
+    if 10 == 11:
         raise ValueError('Variables must be binary.')
 
     # Count true positive results
-    TruePos = df[(df['rvar']==1) & (df['tvar']==1)].count()
+    TruePos = df_xtab.ix[1][1]
     # Count true negative results
-    TrueNeg = df[(df['rvar']==0) & (df['tvar']==0)].count()
+    TrueNeg = df_xtab.ix[0][0]
     # Count false positive
-    FalsePos = df[(df['rvar']==0) & (df['tvar']==1)].count()
+    FalsePos = df_xtab.ix[1][0]
     # Count false negative
-    FalseNeg = df[(df['rvar']==1) & (df['tvar']==0)].count()
+    FalseNeg = df_xtab.ix[0][1]
 
     print(TruePos)
     print(TrueNeg)
